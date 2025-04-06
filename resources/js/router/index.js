b8040fa63890b9af75a574406b6861f18ca18285
tabchoura <mariiembchir@gmail.com>
2025-04-06 18:05:25 +0100

    update

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,3 +1,4 @@
+// router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/components/Home.vue'
@@ -5,21 +6,21 @@ import Offres from '@/components/Offres.vue'
 import Authentification from '@/components/Authentification.vue'
 import RegisterCandidat from '@/components/RegisterCandidat.vue'
 import RegisterRecruteur from '@/components/RegisterRecruteur.vue'
-import Apropos from '../components/Apropos.vue'
-import CompteCandidat from '../components/CompteCandidat.vue'
-import Monprofile from '../components/Monprofile.vue'
-import Candidature from '../components/Candidature.vue'
+import Apropos from '@/components/Apropos.vue'
+import CompteCandidat from '@/components/CompteCandidat.vue'
+import Monprofile from '@/components/Monprofile.vue'
+import Candidature from '@/components/Candidature.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/offres', name: 'Offres', component: Offres },
   { path: '/authentification', name: 'Authentification', component: Authentification },
-  { path: '/register-candidat', name: 'RegisterCandidat', component: RegisterCandidat },
-  { path: '/register-recruteur', name: 'RegisterRecruteur', component: RegisterRecruteur },
+  { path: '/registercandidat', name: 'RegisterCandidat', component: RegisterCandidat },
+  { path: '/registerrecruteur', name: 'RegisterRecruteur', component: RegisterRecruteur },
   { path: '/apropos', name: 'Apropos', component: Apropos },
-  { path: '/compte-candidat', name: 'CompteCandidat', component: CompteCandidat, props: true },
-  { path: '/mon-profile', name: 'Monprofile', component: Monprofile, props: true },
-  { path: '/candidature', name: 'Candidature', component: Candidature, props: true }
+  { path: '/compte-candidat', name: 'CompteCandidat', component: CompteCandidat },
+  { path: '/mon-profile', name: 'Monprofile', component: Monprofile },
+  { path: '/candidature', name: 'Candidature', component: Candidature }
 ]
 
 const router = createRouter({
